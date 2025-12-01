7233d8632e35f4352249df026463d2b016ad185d
kelly kamau <kamaukelly791@gmail.com>
2025-12-01 11:38:42 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import './App.css'
 import UserLogin from './components/auth/Userlogin'
 import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router'
-
-// import About from './components/'
+import About from './components/About/About'
 import Contact from './components/contact/Contact'
-import Registration from './components/auth/registration'
 import { Admindashboard } from './components/Admin dashboard/Aside/Aside/Dashboard'
 import Landing from './components/Landing'
+import Register from './components/auth/Register'
+import Services from './components/Services/Services'
 
 
 function App() {
@@ -16,26 +16,33 @@ function App() {
      path: '/',
      element: <Landing/>
     },
-    // {
-    //   path: '/about',
-    //   element: <About />
-    // },
     {
-      path: '/contact',
-      element: <Contact />
+      path: '/about',
+      element: <About />
     },
+   
     {
       path: '/userLogin',
       element: <UserLogin />
     },
     {
       path: '/register',
-      element: <Registration />
+      element: <Register />
     },
     {
       path: '/admin/dashboard',
       element: <Admindashboard />
-    }
+    },
+
+     {
+      path: '/Services',
+      element: <Services />
+    },
+
+      {
+      path: '/contact',
+      element: <Contact />
+    },
   ])
 
   return (
